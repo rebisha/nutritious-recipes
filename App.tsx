a1f0da1ca91bfb579fdfecf3a3d80c04ce1698f0
Rebisha Pokharel <rebisha67@gmail.com>
2022-08-01 18:48:24 +1000

    add Sanity and Gatsby to the project

--- App.tsx
+++ App.tsx
@@ -1,28 +1,5 @@
 import React from 'react';
-import Index from './src/component/index';
-import NavBar from './src/component/navbar';
-import { Fonts } from './src/styles/fonts';
-import { View } from 'react-native';
 
-import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
-
-const App = () => {
-  let [fontsLoaded] = useFonts(Fonts);
-
-  if (!fontsLoaded) {
-    return <AppLoading />
-  } else {
-    return (
-      <>
-        <NavBar />
-        <Index name='Rebisha' enthusiasmLevel={1} />
-      </>
-    )
-  }
-  
-}
+const App = () => (<h1>Hello World!!</h1>)
 
 export default App;
-
-    // "@types/react-native": "0.63.4",
